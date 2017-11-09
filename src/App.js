4d5ae9615b7966ac2f2b3523dff94eddd5035240
junhobaik <junhobaik@gmail.com>
2017-11-09 23:38:19 +0900

    Modify App, import Search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+import Search from './components/Search';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Search/>
       </div>
     );
   }
