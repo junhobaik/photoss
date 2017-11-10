27165f176365dc51d66a699edc5a6f8703693e31
junhobaik <junhobaik@gmail.com>
2017-11-10 23:48:46 +0900

    Modify Clock, add live time code

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,25 +1,78 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class Clock extends React.Component {
+class Clock extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
     render(){
+
+        const makeDate = ()=>{
+            const date = this.state.date;
+            let dates = {
+                year: date.getFullYear(),
+                month: date.getMonth() + 1,
+                date: date.getDate(),
+                day: date.toString().substr(0,3),
+                hour: date.getHours(),
+                min: date.getMinutes(),
+                sec: date.getSeconds(),
+                mer: "AM"
+            };
+            if(dates.hour > 12){
+                dates.mer = "PM";
+                dates.hour = dates.hour - 12;
+            }
+            const makeTwoChar = (v) => v.toString().length < 2 ? `0${v}` : v;
+            dates.date = makeTwoChar(dates.date);
+            dates.month = makeTwoChar(dates.month);
+            dates.sec = makeTwoChar(dates.sec);
+
+            console.log(dates);
+            return dates;
+        };
+
+        const dates = makeDate();
+
+
         return (
             <div className={"clock"}>
                 <div className={"clock-wrap"}>
 
                     <div className={"date"}>
-                        <span>2017.12.31</span>
+                        <span>{`${dates.year}.${dates.month}.${dates.date} ${dates.day}`}</span>
                     </div>
 
                     <div className={"time"}>
-                        <span className={"hour"}>12</span>
+                        <span className={"hour"}>{dates.hour}</span>
                         <span className={"hm time-dot"}>:</span>
-                        <span className={"min"}>34</span>
+                        <span className={"min"}>{dates.min}</span>
                         <span className={"mc time-dot"}>:</span>
-                        <span className={"sec"}>56</span>
+                        <span className={"sec"}>{dates.sec}</span>
                     </div>
 
                     <div className={"time-mer"}>
-                        <span>PM</span>
+                        <span>{dates.mer}</span>
                     </div>
                 </div>
             </div>
