f7563591878b2a3853793c39795f2e3accb5500f
junhobaik <junhobaik@gmail.com>
2017-11-29 02:33:57 +0900

    Modify Wallpaper, add image onload event

--- src/components/Wallpaper.js
+++ src/components/Wallpaper.js
@@ -7,52 +7,18 @@ export default class Wallpaper extends Component{
         this.state = {
             cnt: 1,
             size: {
-                width: 800,
-                height: 600
+                width: 1600,
+                height: 900
             },
             method: "tag",
-            name: "night",
+            name: "random"
         }
     }
 
     componentDidMount(){
         console.log("componentDidMount Wallpaper");
 
-        const setWallpaper = (ele, size)=>{
-
-            if(this.state.method === "tag"){
-                ele.style.backgroundImage = `url(https://source.unsplash.com/${size.width}x${size.height}/?${this.state.name})`
-            } else if(this.state.method === "collection"){
-                ele.style.backgroundImage = `url(https://source.unsplash.com/${this.state.name}/${size.width}x${size.height})`
-            } else if(this.state.method === "user"){
-
-            } else {
-                console.log("error setWallpaper");
-            }
-        };
-
-        const firstSet = (size)=>{
-            console.log("firstSet");
-            document.querySelector('.first-cover').style.opacity = 1;
-            document.querySelector('.front').style.opacity = 1;
-            document.querySelector('.middle').style.opacity = 0;
-            document.querySelector('.back').style.opacity = 0;
-
-            setWallpaper(document.querySelector('.front'), {width: size.width, height: size.height - 1});
-            setWallpaper(document.querySelector('.middle'), {width: size.width, height: size.height - 2});
-
-            setTimeout(()=>{
-                this.test = setInterval(()=>{
-                    console.log(document.querySelector('.first-cover').style.opacity);
-                    document.querySelector('.first-cover').style.opacity = document.querySelector('.first-cover').style.opacity - 0.05;
-                    if(document.querySelector('.first-cover').style.opacity <= 0) clearInterval(this.test);
-                }, 100)
-            },5000);
-        };
-        firstSet(this.state.size);
-
-
-        function lighter(counter, setTime, p){
+        const lighter = (counter, setTime, p)=>{
             if(counter > 0){
                 setTimeout(function(){
                     counter--;
@@ -69,12 +35,58 @@ export default class Wallpaper extends Component{
                     lighter(counter, setTime, p);
                 }, setTime);
             }
-        }
+        };
+
+
+        const setWallpaper = (ele, size, cnt)=>{
+            console.log(cnt);
+            if(this.state.method === "tag"){
+                const img = new Image();
+                img.onload = function() {
+                    console.log("finish load");
+                    ele.style.backgroundImage = `url(${this.src})`;
+                    if(cnt !== 0) lighter(20, 50, cnt);
+                };
+
+                if(this.state.name === "random"){
+                    img.src = `https://source.unsplash.com/random/${size.width}x${size.height}/`;
+                } else {
+                    img.src = `https://source.unsplash.com/${size.width}x${size.height}/?${this.state.name}`;
+                }
+
+
+            } else if(this.state.method === "collection"){
+                ele.style.backgroundImage = `url(https://source.unsplash.com/${this.state.name}/${size.width}x${size.height})`
+            } else if(this.state.method === "user"){
+
+            } else {
+                console.log("error setWallpaper");
+            }
+        };
+
+
+        const firstSet = (size)=>{
+            console.log("firstSet");
+            document.querySelector('.first-cover').style.opacity = 1;
+            document.querySelector('.front').style.opacity = 1;
+            document.querySelector('.middle').style.opacity = 0;
+            document.querySelector('.back').style.opacity = 0;
+
+            setWallpaper(document.querySelector('.front'), {width: size.width - 10, height: size.height - 20}, 0);
+            setWallpaper(document.querySelector('.middle'), {width: size.width - 20, height: size.height - 10}, 0);
+
+            setTimeout(()=>{
+                this.test = setInterval(()=>{
+                    document.querySelector('.first-cover').style.opacity = document.querySelector('.first-cover').style.opacity - 0.02;
+                    if(document.querySelector('.first-cover').style.opacity <= 0) clearInterval(this.test);
+                }, 50)
+            },3000);
+        };
+        firstSet(this.state.size);
+
 
         setTimeout(()=>{
             this.changer = setInterval(()=>{
-                console.log("changer interval");
-
                 clearInterval(this.test);
 
                 let i = this.state.cnt;
@@ -82,14 +94,14 @@ export default class Wallpaper extends Component{
 
                 if(i === 101) i = 1;
 
-                lighter(20, 50, i);
+                //lighter(20, 50, i);
 
                 if(i%3 === 1){
-                    setWallpaper(document.querySelector('.back'), size);
+                    setWallpaper(document.querySelector('.back'), size, i);
                 }else if(i%3 === 2){
-                    setWallpaper(document.querySelector('.front'), size);
+                    setWallpaper(document.querySelector('.front'), size, i);
                 }else {
-                    setWallpaper(document.querySelector('.middle'), size);
+                    setWallpaper(document.querySelector('.middle'), size, i);
                 }
 
                 this.setState({
@@ -99,8 +111,8 @@ export default class Wallpaper extends Component{
                         height: size.height + 1
                     }
                 })
-            }, 15000);
-        },6000);
+            }, 10000);
+        },5000);
     }
 
     componentWillUnmount(){
@@ -120,15 +132,9 @@ export default class Wallpaper extends Component{
                     <p className={"first-cover-msg"}>Hello world!</p>
                 </div>
 
-                <div
-                    className="front"
-                />
-                <div
-                    className="middle"
-                />
-                <div
-                    className="back"
-                />
+                <div className="front"/>
+                <div className="middle"/>
+                <div className="back"/>
             </div>
         );
     }
