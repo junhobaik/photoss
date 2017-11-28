2e820674c7ec8ff6aa894a518af666a40dd9cb2c
junhobaik <junhobaik@gmail.com>
2017-11-29 01:07:20 +0900

    Modify Wallpaper

--- src/components/Wallpaper.js
+++ src/components/Wallpaper.js
@@ -5,35 +5,51 @@ export default class Wallpaper extends Component{
         super(props);
 
         this.state = {
+            cnt: 1,
             size: {
-                width: 1600,
-                height: 900
-            }
+                width: 800,
+                height: 600
+            },
+            method: "tag",
+            name: "night",
         }
-    }
-
-
-    componentDidUpdate(){
-        console.log("componentDidUpdate Wallpaper");
     }
 
     componentDidMount(){
         console.log("componentDidMount Wallpaper");
 
-        let i = 1;
+        const setWallpaper = (ele, size)=>{
+
+            if(this.state.method === "tag"){
+                ele.style.backgroundImage = `url(https://source.unsplash.com/${size.width}x${size.height}/?${this.state.name})`
+            } else if(this.state.method === "collection"){
+                ele.style.backgroundImage = `url(https://source.unsplash.com/${this.state.name}/${size.width}x${size.height})`
+            } else if(this.state.method === "user"){
+
+            } else {
+                console.log("error setWallpaper");
+            }
+        };
 
         const firstSet = (size)=>{
             console.log("firstSet");
+            document.querySelector('.first-cover').style.opacity = 1;
             document.querySelector('.front').style.opacity = 1;
             document.querySelector('.middle').style.opacity = 0;
             document.querySelector('.back').style.opacity = 0;
-            document.querySelector('.front').style.backgroundImage = `url(https://source.unsplash.com/random/${size.width+1}x${size.height}`;
-            document.querySelector('.middle').style.backgroundImage = `url(https://source.unsplash.com/random/${size.width+2}x${size.height}`;
-            document.querySelector('.back').style.backgroundImage = `url(https://source.unsplash.com/random/${size.width+3}x${size.height}`;
+
+            setWallpaper(document.querySelector('.front'), {width: size.width, height: size.height - 1});
+            setWallpaper(document.querySelector('.middle'), {width: size.width, height: size.height - 2});
+
+            setTimeout(()=>{
+                this.test = setInterval(()=>{
+                    console.log(document.querySelector('.first-cover').style.opacity);
+                    document.querySelector('.first-cover').style.opacity = document.querySelector('.first-cover').style.opacity - 0.05;
+                    if(document.querySelector('.first-cover').style.opacity <= 0) clearInterval(this.test);
+                }, 100)
+            },5000);
         };
-
         firstSet(this.state.size);
-
 
 
         function lighter(counter, setTime, p){
@@ -55,30 +71,36 @@ export default class Wallpaper extends Component{
             }
         }
 
-        this.changer = setInterval(()=>{
-            if(i === 101) i = 1;
-            lighter(20, 75, i++);
+        setTimeout(()=>{
+            this.changer = setInterval(()=>{
+                console.log("changer interval");
 
-            const size = this.state.size;
+                clearInterval(this.test);
 
-            if(i%3 === 1){
-                document.querySelector('.back').style.backgroundImage = `url(https://source.unsplash.com/random/${size.width}x${size.height}`
-            }else if(i%3 === 2){
-                document.querySelector('.front').style.backgroundImage = `url(https://source.unsplash.com/random/${size.width}x${size.height}`
-            }else {
-                document.querySelector('.middle').style.backgroundImage = `url(https://source.unsplash.com/random/${size.width}x${size.height}`
-            }
+                let i = this.state.cnt;
+                const size = this.state.size;
 
-            this.setState({
-                size : {
-                    width: size.width,
-                    height: size.height + 1
+                if(i === 101) i = 1;
+
+                lighter(20, 50, i);
+
+                if(i%3 === 1){
+                    setWallpaper(document.querySelector('.back'), size);
+                }else if(i%3 === 2){
+                    setWallpaper(document.querySelector('.front'), size);
+                }else {
+                    setWallpaper(document.querySelector('.middle'), size);
                 }
-            })
 
-        }, 15000);
-
-
+                this.setState({
+                    cnt: i + 1,
+                    size : {
+                        width: size.width,
+                        height: size.height + 1
+                    }
+                })
+            }, 15000);
+        },6000);
     }
 
     componentWillUnmount(){
@@ -90,9 +112,14 @@ export default class Wallpaper extends Component{
     render(){
         console.log("render Wallpaper");
 
-
         return(
             <div className={"Wallpaper"}>
+
+                <div className="first-cover">
+                    <div className={"space"}/>
+                    <p className={"first-cover-msg"}>Hello world!</p>
+                </div>
+
                 <div
                     className="front"
                 />
