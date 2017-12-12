9bd9e0489ce9b8314bd16fb8212303a086adb21b
junhobaik <junhobaik@gmail.com>
2017-12-12 15:40:58 +0900

    Add package, install redux

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+const counterInitialState = {
+    name: "random"
+};
+
+const reducer = (state = counterInitialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+
+export default reducer;
